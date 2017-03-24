f94d60331c4f23906b1050cf6bcef824f2d8b5ef
courtney <coa.mcallister@gmail.com>
2017-03-24 19:59:13 -0400

    moved the route configuration to a separate file for organization

---
+++ public/js/routes.js
@@ -0,0 +1,20 @@
+(function(){
+  angular.module('mymeanblog').config(RouterConfig);
+
+  RouterConfig.$inject = ['$routeProvider'];
+
+  function RouterConfig($routeProvider){
+    $routeProvider
+      .when('/', {
+        controller: 'SignupController',
+        templateUrl: 'html/views/signup.html'
+      })
+      .when('/login', {
+        controller: 'LoginController',
+        templateUrl: 'html/views/login.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }
+})()
